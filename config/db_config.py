8e4bb81ad0d71bb436c5cf58c921848e3566bd5b
Akshay <93537388+Pheoau1tre@users.noreply.github.com>
2025-09-28 16:33:07 +0400

    Update db_config.py

--- config/db_config.py
+++ config/db_config.py
@@ -1,4 +1,6 @@
 import mysql.connector as mysql
+from utils.logger import Logger
+logger = Logger()
 
 DB_HOST = "localhost"                  
 DB_USER = "root"
@@ -7,12 +9,15 @@ DB_PASSWORD = "Akshay@2008"
 def get_db_connection(db_name = None):
     try:
         connection = mysql.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=db_name
+            host = DB_HOST,
+            user = DB_USER,
+            password = DB_PASSWORD,
+            database = db_name
         )
         return connection
     except mysql.Error as err:
-        print(f"Error connecting to MySQL server: {err}")
+        db_info = db_name if db_name else "no database selected"
+        # Log the error
+        logger.error(f"Error connecting to MySQL server for {db_info}: {err}", context = "Database Connection")
+        print(f"Error connecting to MySQL server for {db_info}: {err}")
         return None
